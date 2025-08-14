77f1e33c026e1afaf9649815dbb16ec0ca20a461
Christophe Avonture <cavo789@yahoo.com>
2025-08-14 12:13:21 +0200

    refactor: homepage (all cards on the same line on desktop)

--- src/components/HomeCard/index.js
+++ src/components/HomeCard/index.js
@@ -5,8 +5,10 @@ import CardBody from '@site/src/components/Card/CardBody';
 import CardImage from '@site/src/components/Card/CardImage';
 
 export default function HomeCard({ title, image, link, description }) {
+  // use col--2 for all cards on the same row row
+  // by using col--4, three cards will be displayed in a row
   return (
-    <div className="col col--4 margin-bottom--lg">
+    <div className="col col--2 margin-bottom--lg">
       <a href={link}>
         <Card>
           <CardImage cardImageUrl={`/img/homepage/${image}`} />
