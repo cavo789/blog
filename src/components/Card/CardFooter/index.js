dd7e587f8706d97eca6efd762702f05143a0a837
Christophe Avonture <cavo789@yahoo.com>
2025-09-09 18:35:35 +0200

    feature: add the ability to read source files from disk / Snippet

--- src/components/Card/CardFooter/index.js
+++ src/components/Card/CardFooter/index.js
@@ -1,30 +1,30 @@
-import React,  { CSSProperties } from 'react';
-import styles from '../styles.module.css';
+import React, { CSSProperties } from "react";
+import styles from "../styles.module.css";
 
-import clsx from 'clsx';
+import clsx from "clsx";
 const CardFooter = ({
   className,
   style,
   children,
   textAlign,
   variant,
-  italic = false ,
+  italic = false,
   noDecoration = false,
   transform,
   truncate = false,
   weight,
 }) => {
-  const text = textAlign ? `text--${textAlign}` :'';
-  const textColor = variant ? `text--${variant}` : '';
-  const textItalic = italic ? 'text--italic' : '';
-  const textDecoration = noDecoration ? 'text-no-decoration' : '';
-  const textType = transform ? `text--${transform}` : '';
-  const textTruncate = truncate ? styles.truncate : '';
-  const textWeight = weight ? `text--${weight}` : '';
+  const text = textAlign ? `text--${textAlign}` : "";
+  const textColor = variant ? `text--${variant}` : "";
+  const textItalic = italic ? "text--italic" : "";
+  const textDecoration = noDecoration ? "text-no-decoration" : "";
+  const textType = transform ? `text--${transform}` : "";
+  const textTruncate = truncate ? styles.truncate : "";
+  const textWeight = weight ? `text--${weight}` : "";
   return (
     <div
       className={clsx(
-        'card__footer',
+        "card__footer",
         className,
         text,
         textType,
@@ -39,5 +39,5 @@ const CardFooter = ({
       {children}
     </div>
   );
-}
+};
 export default CardFooter;
--- src/components/Card/CardHeader/index.js
+++ src/components/Card/CardHeader/index.js
@@ -1,43 +1,43 @@
-  import React,  { CSSProperties } from 'react'; // CSSProperties allows inline styling with better type checking.
-  import clsx from 'clsx'; // clsx helps manage conditional className names in a clean and concise manner.
-  import styles from '../styles.module.css';
+import React, { CSSProperties } from "react"; // CSSProperties allows inline styling with better type checking.
+import clsx from "clsx"; // clsx helps manage conditional className names in a clean and concise manner.
+import styles from "../styles.module.css";
 
-  const CardHeader = ({
-    className, // classNamees for the container card
-    style, // Custom styles for the container card
-    children, // Content to be included within the card
-    textAlign,
-    variant,
-    italic = false ,
-    noDecoration = false,
-    transform,
-    truncate = false,
-    weight,
-  }) => {
-    const text = textAlign ? `text--${textAlign}` :'';
-    const textColor = variant ? `text--${variant}` : '';
-    const textItalic = italic ? 'text--italic' : '';
-    const textDecoration = noDecoration ? 'text-no-decoration' : '';
-    const textType = transform ? `text--${transform}` : '';
-    const textTruncate = truncate ? styles.truncate : '';
-    const textWeight = weight ? `text--${weight}` : '';
-    return (
-      <div
-        className={clsx(
-          'card__header',
-          className,
-          text,
-          textType,
-          textColor,
-          textItalic,
-          textDecoration,
-          textTruncate,
-          textWeight
-        )}
-        style={style}
-      >
-        {children}
-      </div>
-    );
-  }
-  export default CardHeader;
+const CardHeader = ({
+  className, // className for the container card
+  style, // Custom styles for the container card
+  children, // Content to be included within the card
+  textAlign,
+  variant,
+  italic = false,
+  noDecoration = false,
+  transform,
+  truncate = false,
+  weight,
+}) => {
+  const text = textAlign ? `text--${textAlign}` : "";
+  const textColor = variant ? `text--${variant}` : "";
+  const textItalic = italic ? "text--italic" : "";
+  const textDecoration = noDecoration ? "text-no-decoration" : "";
+  const textType = transform ? `text--${transform}` : "";
+  const textTruncate = truncate ? styles.truncate : "";
+  const textWeight = weight ? `text--${weight}` : "";
+  return (
+    <div
+      className={clsx(
+        "card__header",
+        className,
+        text,
+        textType,
+        textColor,
+        textItalic,
+        textDecoration,
+        textTruncate,
+        textWeight
+      )}
+      style={style}
+    >
+      {children}
+    </div>
+  );
+};
+export default CardHeader;
