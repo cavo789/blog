52a05da1bd123478d3d8e009d7ec138bed75b079
Christophe Avonture <cavo789@yahoo.com>
2026-08-28 11:38:04 +0000

    doc: Add ProjectSetup for BlueSky articles

---
+++ blog/2025/08/18/docusaurus-bluesky-comments/files/docusaurus.config.js
@@ -0,0 +1,16 @@
+// Partial file; illustration of what you've to do
+
+// [...]
+
+const config = {
+
+  // [...]
+
+  customFields: {
+    bluesky: {
+      // This is the Bluesky handle as displayed in your Bluesky profile page
+      handle: "your-bluesky-handle",
+    },
+  }
+
+}
